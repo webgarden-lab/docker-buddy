92bf1fec23b1a371fe413b1a4f045885ee70ff14
Lukáš Klíma <arziel12@gmail.com>
2018-08-28 11:49:34 +0200

    Update and rename toolbox.sh to processUrl.sh

---
+++ php/processUrl.sh
@@ -0,0 +1,10 @@
+#!/usr/env bash
+
+BRANCH="$1"
+STAGING_BASE_URL="$2"
+
+## Replace dots and slashes from branch name to dashes
+URL="${BRANCH//./-}"
+URL="${URL//\\//-}"
+
+echo "aym-$URL.${STAGING_BASE_URL}" 
