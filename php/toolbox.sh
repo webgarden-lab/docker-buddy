41638b0f158f253470096093a2a7408e2d01d2bd
Lukáš Klíma <arziel12@gmail.com>
2018-08-28 11:48:11 +0200

    Update toolbox.sh

--- php/toolbox.sh
+++ php/toolbox.sh
@@ -1,2 +1,12 @@
 #!/usr/env bash
 
+processUrl () { 
+  BRANCH="$1"
+  STAGING_BASE_URL="$2"
+  
+  ## Replace dots and slashes from branch name to dashes
+  URL="${BRANCH//./-}"
+  URL="${URL//\\//-}"
+  
+  echo "aym-$URL.${STAGING_BASE_URL}" 
+}
